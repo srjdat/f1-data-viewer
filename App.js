683d9d1f7593b903063d143d5433ebfd4bb85c9a
fish <70816109+srjdat@users.noreply.github.com>
2023-10-09 17:41:40 +0000

    tried to hide the div

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 const log = console.log;
 const yearSelect = document.querySelector(`[id="year"]`);
 
+
 yearSelect.addEventListener(`change`, (e) => {
     const select = e.target; 
     const desc = select.selectedOptions[0].text;
 
-   alert(desc); 
+    //alert(desc);
+
+    let element = document.getElementById(2022);
+    let hidden = element.getAttribute("hidden");
+
+    if (hidden) {
+        element.removeAttribute("hidden"); 
+    } else {
+        element.setAttribute("hidden", "hidden");
+    }
 });
+
+
 
