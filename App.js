c6329a933506fd463e7a9c647583210594b10111
srjdat <srjdatta7@gmail.com>
2023-10-09 23:22:14 -0500

    idk

--- App.js
+++ App.js
@@ -4,17 +4,16 @@ const yearSelect = document.querySelector(`[id="year"]`);
 
 yearSelect.addEventListener(`change`, (e) => {
     const select = e.target; 
-    const desc = select.selectedOptions[0].text;
+    const desc = String(select.selectedOptions[0].text);
 
     //alert(desc);
 
-    let element = document.getElementById(2022);
-    let hidden = element.getAttribute("hidden");
-
-    if (hidden) {
-        element.removeAttribute("hidden"); 
+    let element = document.getElementById('' + desc);
+    
+    if (element.style.display === "none") {
+        element.style.display = "block";
     } else {
-        element.setAttribute("hidden", "hidden");
+        element.style.display = "none";
     }
 });
 
