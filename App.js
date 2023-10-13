65eaa80820ec95094fe06f3c0bad9b7ecfa0ae5a
fish <70816109+srjdat@users.noreply.github.com>
2023-10-13 14:35:47 +0000

    figured it out

--- App.js
+++ App.js
@@ -2,19 +2,27 @@ const log = console.log;
 const yearSelect = document.querySelector(`[id="year"]`);
 
 
+
 yearSelect.addEventListener(`change`, (e) => {
     const select = e.target; 
     const desc = String(select.selectedOptions[0].text);
 
-    //alert(desc);
+    //const winners = getWinners();
+    //const winnerArr = winners["winners"];
 
-    let element = document.getElementById('' + desc);
+    //winnerArr.for_each((e) => {
+    //    document.getElementById("year").innerHTML += `<option value="${e}">${e}</option>`
+    //})
+
+    //const response = getYearWinner(desc);
+    //const winner = response["winner"];
+
     
-    if (element.style.display === "none") {
-        element.style.display = "block";
-    } else {
-        element.style.display = "none";
+
+    if(document.getElementById("yearSelect").innerHTML !== "") {
+        document.getElementById("yearSelect").innerHTML = `<div id="${desc}">Winner: ${desc}</div>`;
     }
+
 });
 
 
