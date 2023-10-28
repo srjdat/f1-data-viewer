aa1375964b5df5d6015a8310328b866271073fea
srjdat <srjdatta7@gmail.com>
2023-10-27 22:53:54 -0500

    loading + ui changes

--- App.js
+++ App.js
@@ -25,22 +25,24 @@ yearSelect.addEventListener(`change`, (e) => {
         url += '/constructorStandings.json';
     }
 
-    document.getElementById("loading").innerHTML += `${"loading"}`; 
+    document.getElementById("year-select").innerHTML = `<div class="load-anim">${"loading..."}</div>`; 
     //alert(url);
     
     fetch(url)
         .then(response => response.text())
         .then(data => {
-            document.getElementById("yearSelect").innerHTML = '';
+            document.getElementById("year-select").innerHTML = '';
             const obj = JSON.parse(data); 
             
+            //erase the loading thing
+            document.getElementById("year-select").innerHTML = `<div id="standings"></div>`; 
             if (temp == 'Drivers') {
                 //for loop to display all the drivers 
                 for (let index = 0; index < obj.MRData.StandingsTable.StandingsLists[0].DriverStandings.length; index++) {
                     const driver = obj.MRData.StandingsTable.StandingsLists[0].DriverStandings[index];
 
                     //get the position then their first name, last name, the team they drive for, and the amount of points they have
-                    document.getElementById("yearSelect").innerHTML += `<div> ${driver.position + ": " + driver.Driver.givenName + " " + driver.Driver.familyName + ": " + driver.Constructors[0].name + " - " + driver.points}</div>`;
+                    document.getElementById("standings").innerHTML += `<div> ${driver.position + ": " + driver.Driver.givenName + " " + driver.Driver.familyName + ": " + driver.Constructors[0].name + " - " + driver.points}</div>`;
                 }
             } else {
                 //for loop to display all the drivers 
@@ -49,11 +51,9 @@ yearSelect.addEventListener(`change`, (e) => {
 
                     //get the position, name of the team, and the points 
                     //TODO: maybe get the names of the drivers that drive for them? 
-                    document.getElementById("yearSelect").innerHTML += `<div> ${constructor.position + ": " + constructor.Constructor.name + " - " + constructor.points}</div>`;
+                    document.getElementById("standings").innerHTML += `<div> ${constructor.position + ": " + constructor.Constructor.name + " - " + constructor.points}</div>`;
                 }
             }
-            console.log(obj);
-            document.getElementById("loading").innerHTML = ''; 
         })
 });
 
