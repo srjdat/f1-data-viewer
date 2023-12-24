e4ae1b43234e5ed04b68facdaee75d6579f0ab79
srjdat <srjdatta7@gmail.com>
2023-12-24 00:32:58 -0600

    new table

--- App.js
+++ App.js
@@ -58,45 +58,55 @@ function standings() {
             //display data
             document.getElementById("year-select").innerHTML = '';
             const obj = JSON.parse(data); 
+            console.log(obj);
             
             //erase the loading thing
             document.getElementById("year-select").innerHTML = `<div class="standings" id="standings"></div>`; 
             if (temp == 'Drivers') {
                 //title for the standings 
                 document.getElementById("standings").innerHTML += `<a href="https://www.formula1.com/en/results.html/${desc}/drivers.html" target="_blank"><div class="title">Driver Standings for ${desc}</div></a>`;
-                document.getElementById("standings").innerHTML += `<table id="standings-table" style="width:auto;"><tr><th>Rank</th><th>Drivers</th><th>Points</th></tr></table>`;
+                document.getElementById("standings").innerHTML += `<table id="standings-table" style="width:fit-content;margin-left:auto;margin-right:auto;"><tbody id="standings-table-body"><tr><th>Rank</th><th>Drivers</th><th>Points</th></tr><tbody></table>`;
 
 
                 //for loop to display all the drivers 
                 for (let index = 0; index < obj.MRData.StandingsTable.StandingsLists[0].DriverStandings.length; index++) {
                     const driver = obj.MRData.StandingsTable.StandingsLists[0].DriverStandings[index];
 
-                    //putting the drivers and the points in an array 
+                    //putting the drivers and the points in an array for the bar graph
                     xValues[index] = driver.Driver.givenName + " " + driver.Driver.familyName; 
                     yValues[index] = driver.points;
 
+                    //NEW WAY OF DISPLAYING IT
                     //make the table
-                    document.getElementById("standings-table").innerHTML += `<tr><td>${driver.position}</td><td>${driver.Driver.givenName + " " + driver.Driver.familyName}</td><td>${driver.points + " points"}</td></tr>`
+                    document.getElementById("standings-table-body").innerHTML += `<tr><td>${driver.position}</td><td>${driver.Driver.givenName + " " + driver.Driver.familyName}</td><td>${driver.points + " points"}</td></tr>`
                     //document.getElementById("standings").innerHTML += `<table id="standings-table" style="100%"></table>`;
 
 
+                    //OLD WAY OF DISPLAYING IT
                     //get the position then their first name, last name, the team they drive for, and the amount of points they have
                     //document.getElementById("standings").innerHTML += `<div> ${driver.position + ". " + driver.Driver.givenName + " " + driver.Driver.familyName + " - " + driver.points} points</div>`;
                 }
             } else {
                 //title for the standings
                 document.getElementById("standings").innerHTML += `<a href="https://www.formula1.com/en/results.html/${desc}/team.html" target="_blank"><div class="title">Constructor Standings for ${desc}</div></a>`;
+                document.getElementById("standings").innerHTML += `<table id="standings-table" style="width:fit-content;margin-left:auto;margin-right:auto;"><tbody id="standings-table-body"><tr><th>Rank</th><th>Constructors</th><th>Points</th></tr><tbody></table>`;
 
                 //for loop to display all the drivers 
                 for (let index = 0; index < obj.MRData.StandingsTable.StandingsLists[0].ConstructorStandings.length; index++) {
                     const constructor = obj.MRData.StandingsTable.StandingsLists[0].ConstructorStandings[index]; 
 
-                    //putting the constructors and the points in an array 
+                    //putting the constructors and the points in an array for the bar graph
                     xValues[index] = constructor.Constructor.name; 
                     yValues[index] = constructor.points; 
 
+                    //NEW WAY OF DISPLAYING IT 
+                    //make each row for the table by getting the information from the array
+                    document.getElementById("standings-table-body").innerHTML += `<tr><td>${constructor.position}</td><td>${constructor.Constructor.name}</td><td>${constructor.points + " points"}</td></tr>`;
+
+
+                    //OLD WAY OF DISPLAYING IT
                     //get the position, name of the team, and the points  
-                    document.getElementById("standings").innerHTML += `<div> ${constructor.position + ". " + constructor.Constructor.name + " - " + constructor.points} points</div>`;
+                    //document.getElementById("standings").innerHTML += `<div> ${constructor.position + ". " + constructor.Constructor.name + " - " + constructor.points} points</div>`;
                 }
             }
 
