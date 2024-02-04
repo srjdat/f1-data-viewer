145a87a71c323f2bf02b7a8e108d6fa7d9fa0191
srjdat <srjdatta7@gmail.com>
2024-02-03 23:16:42 -0600

    icon and home page

--- home.js
+++ home.js
@@ -1,9 +1,24 @@
 document.getElementById("home-text").innerHTML += "test"; 
 
-var url = "https://en.wikipedia.org/w/api.php?action=parse&page=Pet_door&format=json";
+var url = "https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=Formula_One&limit=5";
+//https://www.mediawiki.org/w/api.php?action=parse&format=json&formatversion=2&titles=Pet_door
+//https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=Formula_One&limit=5
+
 
 fetch(url)
-    .then(response => response.text())
+    .then(response => response.json())
     .then(data => {
-        console.log(JSON.parse(data));  
-    }).catch(error => alert(error));
+        console.log(data);
+    }).catch(error => console.log(error));
+
+
+
+/*
+fetch("https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=Belgium&limit=5").then(function(resp) {
+    console.log(resp);
+    return resp.json()
+}).then(function(data) {
+    console.log(data);
+})
+*/
+
