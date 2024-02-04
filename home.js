07714914f6361addfe7e5eb9465c97dcd69c0472
YeaButActuallyNo <srjdatta7@gmail.com>
2024-02-04 17:03:10 -0600

    api

--- home.js
+++ home.js
@@ -2,12 +2,24 @@ document.getElementById("home-text").innerHTML += "test";
 
 //right now all this does is give me five results 
 //i have to get the information from one of them and output it to the user
-var url = "https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=Formula_One&limit=5";
-//https://www.mediawiki.org/w/api.php?action=parse&format=json&formatversion=2&titles=Pet_door
+let url = "https://www.mediawiki.org/w/api.php";
+let params = "?action=query"
+    + "&prop=extracts"
+    + "exlimit=1"
+    + "&exsentences=2"
+    + "&titles=Formula_One"
+    + "explaintext=1"
+    + "&format=json"
+    + "&formatversion=2"
+    + "&origin=*";
+
+
+const wikiurl = url + params; 
+//
 //https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=Formula_One&limit=5
 
 //asyncronous right now and things of that nature and i still have to figure out how to get the articles
-fetch(url)
+fetch(wikiurl)
     .then(function(response){
         console.log(response);
         return response.json()
